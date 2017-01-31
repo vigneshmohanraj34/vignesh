462eb8f497928e4a35dd48b5345ad68a510a802d
vigneshmohanraj34 <nebulamohan@gmail.com>
2017-01-31 17:59:07 +0530

    Create number7.c

---
+++ number7.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+     char a[10];
+     int i=0;
+     printf("enter the string:");
+     scanf("%s",a);
+     for(i=0;i<=4;i++)
+     printf("hello");
+	return 0;
+}
