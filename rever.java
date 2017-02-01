e5b080e88202f349e64b71b371d30942802ccb7a
vigneshmohanraj34 <nebulamohan@gmail.com>
2017-02-01 18:12:59 +0530

    Create rever.java

---
+++ rever.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class rever {
+	public static void main (String[] args) {
+		Scanner s=new Scanner(System.in);
+		StringBuffer s1= new StringBuffer(s.next());
+		System.out.print(s1.reverse());
+	}
+}
