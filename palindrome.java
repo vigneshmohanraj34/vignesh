bc2fd856cb0e6ff1c7a3d0ffbcc1d84f6b3956e7
vigneshmohanraj34 <nebulamohan@gmail.com>
2017-02-04 17:54:19 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class palindrome {
+	public static void main (String[] args) {
+		Scanner s=new Scanner(System.in);
+		String s1=s.next(),s6="",s3="";
+		for(int i=0;i<=s1.length()-1;i++)
+	    s3=s1.substring(i,i+1)+s3;
+		for(int i=0;i<s1.length();i++)
+		 for(int j=i+1;j<=s1.length();j++)
+		  if(s1.substring(i,j).equals(s3.substring(s3.length()-j,s3.length()-i)))
+		 if(s1.substring(i,j).length()>s6.length())
+		 s6=s1.substring(i,j);
+	System.out.println(s6);
+		}}
