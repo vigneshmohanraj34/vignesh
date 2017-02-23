8024fe445d7bbd76785f9120dd3644059195c777
vigneshmohanraj34 <nebulamohan@gmail.com>
2017-02-23 16:45:01 +0530

    Update prime.c

--- prime.c
+++ prime.c
@@ -3,11 +3,11 @@
  int main() {
      static int a;
      scanf("%d",&a);
-if(a==1)
-printf("compound number");
-else
- if(((a%2!=0)&&(a%3!=0)&&(a%5!=0))||(a==2 ||a==3 ||a==5))
-    printf("prime");
+     a=abs(a);
+ if((a==2 ||a==3 ||a==5))
+ printf("prime");
+ else if((a%2!=0)&&(a%3!=0)&&(a%5!=0))
+ printf("prime");
      else
      printf("not");
 	return 0;
