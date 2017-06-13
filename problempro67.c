7f89b5db00eed8a75d141e142b3f4cb336e09b3e
vigneshmohanraj34 <nebulamohan@gmail.com>
2017-06-13 22:18:30 +0530

    Create problempro67.c

--- problempro67.c
+++ problempro67.c
@@ -3,12 +3,12 @@ int main() {
 	int n,f,i,k=0;
 	char s[30];
 	scanf("%d%d",&n,&f);
-	int a[f];
+	int a;
 	scanf("%s",s);
 	for(i=0;i<f;i++)
 	{
-	scanf("%d",&a[i]);
-	if(s[a[i]-1]=='0')
+	scanf("%d",&a);
+	if(s[a-1]=='0')
 	{
 	k=1;
 	break;
