cec8fd5e60c24906ac97884367dca95b6218c20a
vigneshmohanraj34 <nebulamohan@gmail.com>
2017-07-23 17:13:25 +0530

    Create coins.c

---
+++ coins.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+	int s,j,a;
+	printf("enter the amount:");
+	scanf("%d",&a);
+	if(a<=2)
+	s=a;
+	else if(a>=3 && a<5)
+	s=a%3+a/3;
+	else
+	{
+	    s=a/3;
+	    j=a%3;
+	    if(j>=3)
+	    s=s+(j%3)+(j/3);
+	    else
+	    s=s+j;
+	}
+	printf("count=%d",s);
+	return 0;
+}
