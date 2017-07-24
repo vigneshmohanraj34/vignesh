44060ee2928082c5d5f6b12095d9c1abd9e6bc7e
vigneshmohanraj34 <nebulamohan@gmail.com>
2017-07-24 10:45:37 +0530

    Update bitcombination.java

--- bitcombination.java
+++ bitcombination.java
@@ -1,9 +1,13 @@
-import java.util.*;
-public class bitcombination{
+import java.util.Arrays;
+import java.util.Scanner;
+public class HelloWorld{
 public static void main(String []args){
-        Scanner s=new Scanner(System.in);
+         Scanner s=new Scanner(System.in);
         int k=s.nextInt();
-        Integer value[]=new Integer[(int)Math.pow(2,k)];
-        Arrays.setAll(value,i->i);
-        Arrays.asList(value).forEach(i->System.out.format("%0"+k+"d,",Integer.parseInt(Integer.toBinaryString(i))));
+        String value[]=new String[(int)Math.pow(2,k)];
+        Arrays.setAll(value,i->(Integer.toBinaryString(i).replaceAll("0","").length()+Integer.toBinaryString(i)));
+        Arrays.sort(value);
+        Arrays.setAll(value,i->(value[i].substring(1,value[i].length())));
+        Arrays.setAll(value,i->String.format("%"+k+"s",value[i]).replaceAll(" ","0"));
+      System.out.print(String.join(",", Arrays.asList(value)));
      }}
