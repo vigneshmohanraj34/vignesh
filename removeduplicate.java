15ff9111c01d408ef29c70cd7f9984648dc842a8
vigneshmohanraj34 <nebulamohan@gmail.com>
2017-09-03 22:37:50 +0530

    Create removeduplicate.java

---
+++ removeduplicate.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+class removeduplicate {
+	public static void main (String[] args) {
+		Scanner s=new Scanner(System.in);
+		LinkedHashSet <String> s1=new LinkedHashSet<String>(Arrays.asList(s.nextLine().split("")));
+		System.out.print(s1);
+	}
+}
